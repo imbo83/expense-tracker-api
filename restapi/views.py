f04290677cdcb63b9eeb7bbe91815d862af23c03
eman-jaleel <emaanjaleel@gmail.com>
2021-10-05 20:51:31 +0500

    Added API Key to the views and added it in the request headers of the client used to make test requests

--- restapi/views.py
+++ restapi/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from rest_framework.response import Response
-from restapi import models, serializers
-from django.forms.models import model_to_dict
 from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_api_key.permissions import HasAPIKey
 
+from restapi import models, serializers
+
 # Create your views here.
+
+
 class ExpenseListCreate(ListCreateAPIView):
     serializer_class = serializers.Expense
     queryset = models.Expense.objects.all()
